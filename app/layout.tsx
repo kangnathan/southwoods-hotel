3e48a893db24312300e78b274ea1841294c0058c
kangnathan <karlnathanr@gmail.com>
2025-04-17 19:36:47 +0800

    fix: build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Inter } from "next/font/google"
 import "./globals.css"
 
-const interSans = Geist({
+const interSans = Inter({
   variable: "--font-inter-sans",
   subsets: ["latin"],
 })
